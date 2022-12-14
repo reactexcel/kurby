d8f9b4c9cb0d2e47377e6aa16fe313144cf5844a
Kelson Erwin <kelson@ksensetech.com>
2022-12-14 14:46:43 -0700

    change api key for deploy

--- globals/GLOBAL_SETTINGS.ts
+++ globals/GLOBAL_SETTINGS.ts
@@ -1,6 +1,6 @@
 //TODO split this file into multiples each for it's own purpose
 export default {
-    GOOGLE_MAPS_API_KEY: 'AIzaSyDeEitaPNt1_i6jAnaL-_XScF2h2ZpZ6rE',
+    GOOGLE_MAPS_API_KEY: 'AIzaSyA0SVNLRMnplDZcwIxnjcv4wcMHtYPax30',
     MAP_ZOOM_MILES : 2,
     MILES_TO_METERS: (miles: number)=> miles * 1609.34,
     MAP_ZOOM_DEFAULT: 14,
