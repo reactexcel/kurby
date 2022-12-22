1bd5383b4f703a1ea737168be80efc3840ba3762
Mirko <mirkmark2@gmail.com>
2022-12-22 22:13:21 +0100

    fix nearby images try 3

--- next.config.js
+++ next.config.js
@@ -3,8 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['lh3.googleusercontent.com', '*maps.googleapis.com*'],
+    domains: ['lh3.googleusercontent.com', 'maps.googleapis.com'],
   },
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "maps.googleapis.com",
+    },
+    {
+      protocol: "https",
+      hostname: "lh3.googleusercontent.com",
+    },
+  ],
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
