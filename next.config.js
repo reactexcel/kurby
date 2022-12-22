18e9cae4fbf98eacd474d222288d920257209dff
Mirko <mirkmark2@gmail.com>
2022-12-22 22:25:22 +0100

    fix nearby images try 5

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['lh3.googleusercontent.com', 'maps.googleapis.com', 'app.kurby.ai'],
+    unoptimized: true
   },
   remotePatterns: [
     {
