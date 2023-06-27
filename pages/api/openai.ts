09f890987fd747291398e27c3a887cfa3effdf17
nebo <nebo.radosavovic@gmail.com>
2023-06-27 09:07:15 +0200

    increase token limit; update prompt

--- pages/api/openai.ts
+++ pages/api/openai.ts
@@ -9,16 +9,12 @@ type Data = {
 };
 
 //TODO We should validate the request body using zod or something
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   const { formatted_address: address, geometry } = req.body;
 
   //! Temp validation to prevent fake requests. Should validate incoming request as in todo
   const BAD_REQUEST_CODE = 400;
-  if(!geometry || !Object.keys(geometry).length || !address) res.status(BAD_REQUEST_CODE)
+  if (!geometry || !Object.keys(geometry).length || !address) res.status(BAD_REQUEST_CODE);
 
   //This converts the bullets or dashes into an array that can be used by the front-end
   const bulletsToArray = (str: string | undefined) => {
@@ -42,22 +38,16 @@ export default async function handler(
   };
 
   //Method handles creating a completion with openAI
-  const createCompletionWithAi = async ({
-    prompt,
-    max_tokens,
-  }: {
-    prompt: string;
-    max_tokens: number;
-  }) => {
+  const createCompletionWithAi = async ({ prompt, max_tokens }: { prompt: string; max_tokens: number }) => {
     //Init openAI
 
-  if (!process.env.OPENAI_API_KEY) {
-    console.error("no api key for openai");
-  }
+    if (!process.env.OPENAI_API_KEY) {
+      console.error("no api key for openai");
+    }
 
-  const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
     const openai = new OpenAIApi(configuration);
     const openAiResponse = await openai.createCompletion({
       prompt,
@@ -72,27 +62,25 @@ export default async function handler(
 
   //Prompts to be answered and returned to front-end
   const getOpenAiResponses = async () => {
-    
-
     const explainedLikeLocalResponse = await createCompletionWithAi({
-      prompt: `I'm interested in buying ${address}. Tell me about the location. What is the crime rate? What is the school district rating? What amenities are nearby? What is the average household income in this neighborhood and the nearby neighborhoods? How far is the nearest city center? How's the jobs market? End with a full sentance.`,
-      max_tokens: 150,
+      prompt: `I'm interested in buying ${address}. Tell me about the location. Tell me about the crime rate, the school district rating, and the nearby amenities such as schools, parks, grocery stores, tourist attractions, and more. Tell me about the average household income in this neighborhood and the nearby neighborhoods. If this particular property is not in the city tell me how far the nearest city center is. Tell me about the job growth in this neighborhood. End with a full sentence.`,
+      max_tokens: 500,
     });
-    let lastInd = explainedLikeLocalResponse?.lastIndexOf('.') || 0;
-    const noPartialResponses = explainedLikeLocalResponse?.slice(0, lastInd + 1)
+    let lastInd = explainedLikeLocalResponse?.lastIndexOf(".") || 0;
+    const noPartialResponses = explainedLikeLocalResponse?.slice(0, lastInd + 1);
 
     const greenFlags = await createCompletionWithAi({
       prompt: `Create a bullet list with 4 items of positive aspects of living at ${address}. Do not include the specified address in your response. Do not include any negative aspects in your list. Do not contradict the following information: "${noPartialResponses}"`,
-      max_tokens: 100,
+      max_tokens: 150,
     });
 
     const redFlags = await createCompletionWithAi({
       prompt: `Create a bullet list with 4 items of negative aspects of living at ${address}. Do not include the specified address in your response. Do not include any positive aspects in your list. Do not contradict the following information: "${greenFlags}"`,
-      max_tokens: 100,
+      max_tokens: 150,
     });
 
     return {
-      explained_like_a_local:noPartialResponses,
+      explained_like_a_local: noPartialResponses,
       greenFlags: bulletsToArray(greenFlags),
       redFlags: bulletsToArray(redFlags),
     };
