503b0b2f5bd4045bc572f0727ed64670bac89131
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2023-07-05 13:26:40 +0300

    map the entire us state

--- components/Census/GeoJSON/getCensusCartographic.ts
+++ components/Census/GeoJSON/getCensusCartographic.ts
@@ -9,13 +9,13 @@ export const getCartographicData = async ({}: ICensusCartographicData) => {
       {
         vintage: "2021",
         geoHierarchy: {
-          state: "12",
+          state: "*",
           county: "*",
         },
         sourcePath: ["acs", "acs5"],
         values: ["B19013_001E"], // MEDIAN_HOUSEHOLD_INCOME
         geoResolution: "500k",
-        // statsKey: "<your key here>",
+        statsKey: "978f544e844f821ccfe1dd7620e9180801de2107",
       },
       async (err: any, res: any) => {
         resolve(res);
