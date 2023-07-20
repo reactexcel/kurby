49ca2169e2f8ec57d244568a31b610434f2d92ab
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2023-07-20 15:24:41 +0300

    fix issue

--- components/BodyContent/Property/Property.tsx
+++ components/BodyContent/Property/Property.tsx
@@ -94,7 +94,7 @@ export default function Property({ explainedLikeAlocal }: { explainedLikeAlocal:
         <CircularProgress />
       ) : isAddressInUSA ? (
         <div className={styles.main}>
-          {!Boolean(user) || (isFreePlan && <KurbyPaidPlanLimit type={TabLimitMessage.PROPERTY_DATA_TAB} />)}
+          {isFreePlan && <KurbyPaidPlanLimit type={TabLimitMessage.PROPERTY_DATA_TAB} />}
           <div className={styles.wrapper}>
             <img
               src={
