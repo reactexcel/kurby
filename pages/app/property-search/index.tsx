b524d6b4d5736538836325b0070d6d2a2b34b791
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2023-07-28 21:42:58 +0300

    add property search page support

---
+++ pages/app/property-search/index.tsx
@@ -0,0 +1,11 @@
+import Navbar from "components/Navbar/Navbar";
+import { PropertySearch } from "features/property-search-page/PropertySearchPage";
+
+export default function PropertySearchPage() {
+  return (
+    <>
+      <Navbar />
+      <PropertySearch />
+    </>
+  );
+}
