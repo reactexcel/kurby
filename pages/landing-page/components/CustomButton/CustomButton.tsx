29602235f40bd6fa058de73aa0e5f7daeff04c6b
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2023-09-13 15:54:15 +0300

    commited deleted files

--- pages/landing-page/components/CustomButton/CustomButton.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./CustomButton.module.scss";
-
-export default function CustomButton({ text, font_size = "1rem" }: { text: string; font_size: string }) {
-  return (
-    <button style={{ fontSize: font_size }} className={styles.main}>
-      {text}
-    </button>
-  );
-}
