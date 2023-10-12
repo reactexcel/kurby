358133b44b491e33ad2a7e82ecfaf4de335ba01b
MubashirLabar <61319180+MubashirLabar@users.noreply.github.com>
2023-10-13 00:18:14 +0500

    revert code

--- pages/api/openai.ts
+++ pages/api/openai.ts
@@ -41,6 +41,8 @@ const handler = async (req: any) => {
 
   let isPresetAllowed = ["living", "domesticTourism", "internationalTourism"].includes(preset);
 
+  console.log("token....", token, isPresetAllowed);
+
   if (token && !isPresetAllowed) {
     try {
       const response = await fetch(`${outsetaBaseUrl}/profile?fields=Account.CurrentSubscription.Plan.*`, {
