0e423a8fade42db1ad779b6ff86ea4769d41b951
MubashirLabar <61319180+MubashirLabar@users.noreply.github.com>
2023-11-08 20:57:10 +0500

    fixed build failed issue

--- utils/address.ts
+++ utils/address.ts
@@ -1,6 +1,6 @@
 export const addressToUrl = (formatted_address: string) => {
   if (!formatted_address) {
-    return;
+    return "";
   }
   const encodedAddress = formatted_address.replace(/ #/g, "---").replace(/, /g, "--").replace(/ /g, "-");
   return encodedAddress;
